9e64d492421b591d5127da5325232b7fbca5e8bf
Devansh Rajoria <devanshrajoria@gmail.com>
2017-08-10 22:40:39 -0700

    Update checking_ir_sensor.ino

--- checking_ir_sensor.ino
+++ checking_ir_sensor.ino
@@ -1,29 +1,29 @@
 int ir1 = 2;
-//int ir2 = 7;
-//m1f=12 ,m1b=13 ,m2f=3 ,m2b=2 ;
+int ir2 = 3;
+int ledpin = 13;
 void setup()
 {
   pinMode(ir1,INPUT);
-  /*pinMode(ir2,INPUT);
-  pinMode(m1f,OUTPUT);
-  pinMode(m1f,OUTPUT);
-  pinMode(m1f,OUTPUT);
-  pinMode(m1f,OUTPUT);
-*/
-  Serial.begin(9600); 
+  pinMode(ir2,INPUT);
+  pinMode(ledpin,OUTPUT);
 }
 void loop()
 {
-  ir1=digitalRead(ir1);
+  int ir_1 = digitalRead(ir1);
+  int ir_2 = digitalRead(ir2);
   
-  if(ir1 == 1){
+  if(ir_1 == 1){
      digitalWrite(ledpin,1); 
      delay(200);
      digitalWrite(ledpin,0);
   }
+  if(ir_2 == 1){
+     digitalWrite(ledpin,1); 
+     delay(200);
+     digitalWrite(ledpin,0);
+  } 
   else{
     digitalWrite(ledpin,0);
   }
   
 }
-
